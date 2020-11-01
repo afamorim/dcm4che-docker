8eb65ec7fa535afc66a2edfee88e7b581e20ce84
joaojuniormail <S4lv4c40>
2020-11-01 12:11:48 -0300

    build: criação do cacerts.p12

---
+++ dcm4chee/cert-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+domain=$1
+
+echo $domain
+
+if [ -z "$domain" ]; then
+    echo "Vocë deve adicionar o argumento do dominio"
+	exit;
+fi
+
+cert_path=certs
+
+mkdir $cert_path
+
+sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$domain/cert.pem -inkey /etc/letsencrypt/live/$domain/privkey.pem -certfile /etc/letsencrypt/live/$domain/chain.pem -out $cert_path/key.p12 -name keystore
+sudo cp $cert_path/cacerts-main.p12 $cert_path/cacerts.p12
+sudo keytool -import -alias e-laudos.com \
+	-keystore $cert_path/cacerts.p12 \
+	-file /etc/letsencrypt/live/$domain/cert.pem \
+	-storepass secret \
+	-noprompt
+
+sudo chmod -R 777 $cert_path
---
+++ dcm4chee/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec cert-install.sh

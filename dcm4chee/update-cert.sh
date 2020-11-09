85bf8f6e712bb580a2ca550f72a537ad1ff86d0f
joaojuniormail <S4lv4c40>
2020-11-09 08:21:19 -0300

    fix: verifica a validade do certificado digital

--- dcm4chee/update-cert.sh
+++ dcm4chee/update-cert.sh
@@ -2,8 +2,15 @@
 
 domain=$1
 
+pushd /home/elaudoss/dcm4che-docker/dcm4chee/
+
+days_to_expiry=$(certbot certificates | grep -P '\d+ day' -o | sed "s/ day//")
+if [ $days_to_expiry -gt 10 ]; then
+    echo "Certificado digital continua válido";
+    exit;
+fi
 certbot renew
 cp /etc/letsencrypt/live/$domain/privkey.pem /etc/webmin/letsencrypt-key.pem
-cp /etc/letsencrypt/live/$domain/cert.pem /etc/webmin/letsencrypt-cert.pem
+cp /etc/letsencrypt/live/$domain/fullchain.pem /etc/webmin/letsencrypt-cert.pem
 bash cert-install.sh $domain
 docker-compose -p dcm4chee restart

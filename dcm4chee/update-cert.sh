16d104a92f589b6918d53992850099389d2aebb3
joaojuniormail <S4lv4c40>
2020-11-09 07:09:23 -0300

    fix: corrige a renovação de certificado

--- dcm4chee/update-cert.sh
+++ dcm4chee/update-cert.sh
@@ -2,5 +2,8 @@
 
 domain=$1
 
+certbot renew
+cp /etc/letsencrypt/live/$domain/privkey.pem /etc/webmin/letsencrypt-key.pem
+cp /etc/letsencrypt/live/$domain/cert.pem /etc/webmin/letsencrypt-cert.pem
 bash cert-install.sh $domain
 docker-compose -p dcm4chee restart
